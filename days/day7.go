5e07fb36584b6831914960a1b0d7ea654bfd03c1
Bob Parsons <deadstylebp@gmail.com>
2024-12-08 05:00:32 -0600

    Add channels for performance

--- days/day7.go
+++ days/day7.go
@@ -7,90 +7,129 @@ import (
 	"math/big"
 	"os"
 	"strings"
+	"sync"
 )
 
 func Day7() {
 	data := getDay7Data()
-	calibrationTotals := big.NewInt(0)
-	calibrationsToRetotal := [][]*big.Int{}
+	calibrationTotals := make(chan *big.Int, len(data))
+	calibrationsToRetotal := make(chan []*big.Int, len(data))
+	var wg sync.WaitGroup
 
 	for _, calibrations := range data {
-		expectedTotal := calibrations[0]
-		potentials := []*big.Int{calibrations[1]}
-		for i := 2; i < len(calibrations); i++ {
-			updatedPotentials := []*big.Int{}
-			for _, potential := range potentials {
-				add := new(big.Int).Add(calibrations[i], potential)
-				if add.Cmp(expectedTotal) <= 0 {
-					updatedPotentials = append(updatedPotentials, add)
-				}
-				multiply := new(big.Int).Mul(calibrations[i], potential)
-				if multiply.Cmp(expectedTotal) <= 0 {
-					updatedPotentials = append(updatedPotentials, multiply)
-				}
-				potentials = updatedPotentials
+		wg.Add(1)
+		go func(calibs []*big.Int) {
+			defer wg.Done()
+			if result := processCalibration(calibs); result != nil {
+				calibrationTotals <- result
+			} else {
+				calibrationsToRetotal <- calibs
 			}
-		}
-		found := false
-		for _, result := range potentials {
-			if result.Cmp(expectedTotal) == 0 {
-				calibrationTotals.Add(calibrationTotals, result)
-				found = true
-				break
-			}
-		}
-		if !found {
-			calibrationsToRetotal = append(calibrationsToRetotal, calibrations)
-		}
+		}(calibrations)
 	}
 
-	expandedCalibrationsTotal := big.NewInt(0)
-	for _, calibrations := range calibrationsToRetotal {
-		expectedTotal := calibrations[0]
-		potentials := []*big.Int{calibrations[1]}
+	wg.Wait()
+	close(calibrationsToRetotal)
 
-		for i := 2; i < len(calibrations); i++ {
-			updatedPotentials := []*big.Int{}
-			addToPotentials := func(value *big.Int) {
-				if value.Cmp(expectedTotal) <= 0 {
-					updatedPotentials = append(updatedPotentials, value)
-				}
-			}
-
-			for _, potential := range potentials {
-				add := new(big.Int).Add(calibrations[i], potential)
-				addToPotentials(add)
-
-				multiply := new(big.Int).Mul(calibrations[i], potential)
-				addToPotentials(multiply)
-
-				concat := concatInts(potential, calibrations[i])
-				addToPotentials(concat)
-
-				addConcat := concatInts(potential, add)
-				addToPotentials(addConcat)
-
-				mulConcat := concatInts(potential, multiply)
-				addToPotentials(mulConcat)
-
-				potentials = updatedPotentials
-			}
-		}
-
-		for _, result := range potentials {
-			if result.Cmp(expectedTotal) == 0 {
-				expandedCalibrationsTotal.Add(expandedCalibrationsTotal, result)
-				break
-			}
-		}
+	var retotalSlice [][]*big.Int
+	for calibs := range calibrationsToRetotal {
+		retotalSlice = append(retotalSlice, calibs)
 	}
 
-	totalSum := new(big.Int).Add(calibrationTotals, expandedCalibrationsTotal)
+	expandedTotals := make(chan *big.Int, len(retotalSlice))
+	for _, calibrations := range retotalSlice {
+		wg.Add(1)
+		go func(calibs []*big.Int) {
+			defer wg.Done()
+			if result := procExpandedCalculations(calibs); result != nil {
+				expandedTotals <- result
+			}
+		}(calibrations)
+	}
+
+	wg.Wait()
+	close(calibrationTotals)
+	close(expandedTotals)
+
+	totalSum := big.NewInt(0)
+	expandedSum := big.NewInt(0)
+
+	for result := range calibrationTotals {
+		totalSum.Add(totalSum, result)
+	}
+
+	for result := range expandedTotals {
+		expandedSum.Add(expandedSum, result)
+	}
+
+	finalSum := new(big.Int).Add(totalSum, expandedSum)
 	log.Printf(
 		"Total of calibrations: %s with an expanded Calibration Total of: %s",
-		calibrationTotals.String(),
 		totalSum.String(),
+		finalSum.String(),
 	)
+}
+
+func processCalibration(calibrations []*big.Int) *big.Int {
+	expectedTotal := calibrations[0]
+	potentials := []*big.Int{calibrations[1]}
+
+	for i := 2; i < len(calibrations); i++ {
+		updatedPotentials := []*big.Int{}
+		for _, potential := range potentials {
+			add := new(big.Int).Add(calibrations[i], potential)
+			if add.Cmp(expectedTotal) <= 0 {
+				updatedPotentials = append(updatedPotentials, add)
+			}
+			multiply := new(big.Int).Mul(calibrations[i], potential)
+			if multiply.Cmp(expectedTotal) <= 0 {
+				updatedPotentials = append(updatedPotentials, multiply)
+			}
+			potentials = updatedPotentials
+		}
+	}
+
+	for _, result := range potentials {
+		if result.Cmp(expectedTotal) == 0 {
+			return result
+		}
+	}
+	return nil
+}
+
+func procExpandedCalculations(calibrations []*big.Int) *big.Int {
+	expectedTotal := calibrations[0]
+	potentials := []*big.Int{calibrations[1]}
+
+	for i := 2; i < len(calibrations); i++ {
+		updatedPotentials := []*big.Int{}
+		addToPotentials := func(value *big.Int) {
+			if value.Cmp(expectedTotal) <= 0 {
+				updatedPotentials = append(updatedPotentials, value)
+			}
+		}
+
+		for _, potential := range potentials {
+			add := new(big.Int).Add(calibrations[i], potential)
+			addToPotentials(add)
+			multiply := new(big.Int).Mul(calibrations[i], potential)
+			addToPotentials(multiply)
+			concat := concatInts(potential, calibrations[i])
+			addToPotentials(concat)
+			addConcat := concatInts(potential, add)
+			addToPotentials(addConcat)
+			mulConcat := concatInts(potential, multiply)
+			addToPotentials(mulConcat)
+			potentials = updatedPotentials
+		}
+	}
+
+	for _, result := range potentials {
+		if result.Cmp(expectedTotal) == 0 {
+			return result
+		}
+	}
+	return nil
 }
 
 func concatInts(num1, num2 *big.Int) *big.Int {

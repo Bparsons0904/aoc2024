4033bf6a1466b1c6bbb93dfdbdc541eda4c83a80
Bob Parsons <deadstylep@gmail.com>
2024-12-08 22:10:06 -0600

    day8b complete

--- days/day8.go
+++ days/day8.go
@@ -13,6 +13,7 @@ var nodeRegex = regexp.MustCompile(`\w`)
 func Day8() {
 	data, mapRowLen, mapColLen := getDay8Data()
 	uniqueNodes := []Direction{}
+	uniqueNodesExtended := []Direction{}
 
 	for _, nodes := range data {
 		for i, node := range nodes {
@@ -25,11 +26,60 @@ func Day8() {
 						uniqueNodes = append(uniqueNodes, antiNode)
 					}
 				}
+				antiNodesExtendedToAdd := nodeToCheckExtended(
+					node,
+					nodes[j+1],
+					mapRowLen,
+					mapColLen,
+				)
+				for _, antiNode := range antiNodesExtendedToAdd {
+					if !slices.ContainsFunc(uniqueNodesExtended, func(dir Direction) bool {
+						return dir.Row == antiNode.Row && dir.Col == antiNode.Col
+					}) {
+						uniqueNodesExtended = append(uniqueNodesExtended, antiNode)
+					}
+				}
 			}
 		}
 	}
 
 	log.Println(len(uniqueNodes))
+	log.Println(len(uniqueNodesExtended))
+}
+
+func nodeToCheckExtended(nodeA, nodeB Direction, rowLen, colLen int) []Direction {
+	dif := Direction{nodeA.Row - nodeB.Row, nodeA.Col - nodeB.Col}
+	results := []Direction{}
+
+	i := 1
+	for {
+		toCheck := Direction{
+			nodeA.Row + (i * dif.Row), nodeA.Col + i*(dif.Col),
+		}
+		if isInBounds(toCheck.Row, toCheck.Col, rowLen, colLen) {
+			results = append(results, toCheck)
+			i++
+			continue
+		}
+		break
+	}
+	j := 1
+
+	for {
+		toCheck := Direction{
+			nodeB.Row - (j * dif.Row), nodeB.Col - j*(dif.Col),
+		}
+		if isInBounds(toCheck.Row, toCheck.Col, rowLen, colLen) {
+			results = append(results, toCheck)
+			j++
+			continue
+		}
+		break
+	}
+
+	results = append(results, nodeA)
+	results = append(results, nodeB)
+	return results
 }
 
 func filterDuplicateDirections(directions []Direction) []Direction {

1c1ec89c2ed40f43bb695d3054ccd91687669b6c
Bob Parsons <deadstylebp@gmail.com>
2024-12-09 22:07:32 -0600

    day9b

--- days/day9.go
+++ days/day9.go
@@ -11,16 +11,79 @@ func Day9() {
 
 	diskMap := getDiskMap(data)
 	compressedDisk := compressDisk(diskMap)
+	compressedDiskOptimized := compressedDiskOptimized(diskMap)
 	fileSystemCheckSum := calculateChecksum(compressedDisk)
+	fileSystemOptimizedCheckSum := calculateChecksum(compressedDiskOptimized)
 
 	log.Println(fileSystemCheckSum)
+	log.Println(fileSystemOptimizedCheckSum)
+}
+
+func compressedDiskOptimized(diskMap []int) []int {
+	compressedDisk := make([]int, len(diskMap))
+	copy(compressedDisk, diskMap)
+
+	getLastGroup := func(fileID int) (start int, size int) {
+		lastPos := -1
+		for i := len(compressedDisk) - 1; i >= 0; i-- {
+			if compressedDisk[i] == fileID {
+				lastPos = i
+				break
+			}
+		}
+		if lastPos == -1 {
+			return -1, 0
+		}
+
+		firstPos := lastPos
+		for firstPos >= 0 && compressedDisk[firstPos] == fileID {
+			firstPos--
+		}
+		firstPos++
+
+		return firstPos, lastPos - firstPos + 1
+	}
+
+	getFirstOpenSpot := func(end int, size int) int {
+		for i := 0; i < end; i++ {
+			if compressedDisk[i] == -1 {
+				spaceSize := 0
+				for j := i; j < end && compressedDisk[j] == -1; j++ {
+					spaceSize++
+					if spaceSize == size {
+						return i
+					}
+				}
+			}
+		}
+		return -1
+	}
+
+	for fileID := len(compressedDisk); fileID >= 0; fileID-- {
+		start, size := getLastGroup(fileID)
+		if start == -1 || size == 0 {
+			continue
+		}
+
+		newStart := getFirstOpenSpot(start, size)
+		if newStart != -1 {
+			for i := 0; i < size; i++ {
+				compressedDisk[newStart+i] = fileID
+			}
+			for i := start; i < start+size; i++ {
+				compressedDisk[i] = -1
+			}
+		}
+	}
+
+	return compressedDisk
 }
 
 func calculateChecksum(compressedDisk []int) int {
 	result := 0
 	for i, char := range compressedDisk {
 		if char == -1 {
-			break
+			continue
 		}
 		result += i * int(char)
 	}

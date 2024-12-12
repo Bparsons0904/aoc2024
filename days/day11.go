2217ab13d5c8a5fa6fa06d020fe6ccb3b9c02f6d
Bob Parsons <deadstylebp@gmail.com>
2024-12-11 19:34:00 -0600

    day11b solution

--- days/day11.go
+++ days/day11.go
@@ -10,24 +10,96 @@ import (
 
 func Day11() {
 	data := getDay11Data()
-	plutoStones := viewPlutoStones(data)
-
-	log.Println("stones", plutoStones)
-}
-
-func viewPlutoStones(data []int) int {
-	results := make([]int, len(data))
-	copy(results, data)
-
-	for i := 0; i < 25; i++ {
-		updatedResults := []int{}
-		for j := 0; j < len(results); j++ {
-			updatedResults = append(updatedResults, viewedStone(results[j])...)
-		}
-		results = updatedResults
+	stoneCounts := make(map[int]int)
+	for _, stone := range data {
+		stoneCounts[stone]++
 	}
-	return len(results)
+
+	blinks25 := viewPlutoStonesWithMap(stoneCounts, 25)
+	blinks75 := viewPlutoStonesWithMap(stoneCounts, 75)
+	log.Printf(
+		"Total stones after 25 blinks: %d and %d stones with 75 freaking blinks",
+		getTotalStones(blinks25),
+		getTotalStones(blinks75),
+	)
 }
+
+func viewPlutoStonesWithMap(stoneCounts map[int]int, views int) map[int]int {
+	currentCounts := make(map[int]int)
+	for key, value := range stoneCounts {
+		currentCounts[key] = value
+	}
+
+	for i := 0; i < views; i++ {
+		newCounts := make(map[int]int)
+		for stone, count := range currentCounts {
+			newStones := viewedStone(stone)
+			for _, newStone := range newStones {
+				newCounts[newStone] += count
+			}
+		}
+		currentCounts = newCounts
+	}
+	return currentCounts
+}
+
+func getTotalStones(stoneCounts map[int]int) int {
+	total := 0
+	for _, count := range stoneCounts {
+		total += count
+	}
+	return total
+}
+
+// func viewPlutoStonesTimes3(plutoStones []int) int {
+// 	finalResults := 0
+//
+// 	for _, stone := range plutoStones {
+// 		log.Println("stone", stone)
+// 		finalResults += viewStoneRecursively(stone, 75, 0)
+// 	}
+// 	// for i := 0; i < len(plutoStones); i++ {
+// 	// 	group := viewPlutoStones(plutoStones[i:i+1], 25)
+// 	// 	for j := 0; j < len(group); j++ {
+// 	// 		log.Println("finalResults", j, len(group), len(plutoStones))
+// 	// 		finalResults += len(viewPlutoStones(group[j:j+1], 25))
+// 	// 	}
+// 	// 	log.Println("group", i, len(group), len(plutoStones))
+// 	// }
+// 	return finalResults
+// }
+//
+// func viewStoneRecursively(stone, iteration, result int) int {
+// 	if iteration == 0 {
+// 		return 1
+// 	}
+//
+// 	results := 0
+// 	viewedStones := viewedStone(stone)
+// 	log.Println("viewedStones", viewedStones, iteration, result)
+// 	for _, viewStone := range viewedStones {
+// 		results += viewStoneRecursively(viewStone, iteration-1, result)
+// 	}
+//
+// 	return results
+// }
+//
+// func viewPlutoStones(data []int, views int) []int {
+// 	// log.Println("Starting view", len(data), views)
+// 	results := make([]int, len(data))
+// 	copy(results, data)
+//
+// 	for i := 0; i < views; i++ {
+// 		updatedResults := []int{}
+// 		// log.Printf("Working on round %d, with length of %d", i, len(results))
+// 		// log.Println("Actual array", i, results)
+// 		for j := 0; j < len(results); j++ {
+// 			updatedResults = append(updatedResults, viewedStone(results[j])...)
+// 		}
+// 		results = updatedResults
+// 	}
+// 	return results
+// }
 
 func viewedStone(stone int) []int {
 	if stone == 0 {

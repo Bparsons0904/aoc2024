467e7fbb1c5103d2cd026940f37d42e807567326
Bob Parsons <deadstylep@gmail.com>
2024-12-15 15:02:11 -0600

    WIP day15a movements working

--- days/day15.go
+++ days/day15.go
@@ -37,11 +37,16 @@ var (
 	space  = rune(46)
 	wall   = rune(35)
 	object = rune(79)
+	robot  = rune(64)
 )
 
 func moveWarehouseRobot(warehouse *Warehouse) {
-	for _, movement := range warehouse.Movements {
+	for i, movement := range warehouse.Movements {
+		log.Println("Starting movements", i, movement)
 		handleNextRobotMove(warehouse, movement)
+		// if i > 10 {
+		// 	break
+		// }
 	}
 }
 
@@ -62,15 +67,18 @@ func handleNextRobotMove(warehouse *Warehouse, movement Position) {
 	newSpace := warehouse.Layout[newPosition]
 	switch newSpace {
 	case space:
-		// log.Println("We have an open space", newPosition)
+		log.Println("We have an open space", newPosition)
+		warehouse.Layout[warehouse.RobotPosition] = space
+		warehouse.Layout[newPosition] = robot
 		warehouse.RobotPosition = newPosition
-		handleNextRobotMove(warehouse, movement)
+		printWarehouseLayout(*warehouse)
+		// handleNextRobotMove(warehouse, movement)
 	// case wall:
 	// 	log.Println("We have an wall space")
 	case object:
+		log.Println("We have an object space")
 		pushObject(warehouse, movement, newPosition)
 
-		// log.Println("We have an object space")
 	}
 	// printWarehouseLayout(*warehouse)
 }
@@ -80,21 +88,24 @@ func pushObject(warehouse *Warehouse, movement Position, objectPosition Position
 	spacePastObject := warehouse.Layout[positionPastObject]
 	switch spacePastObject {
 	case space:
-		log.Println("We have an open space", objectPosition)
+		log.Println("We have an open space when trying to push", objectPosition)
 		printWarehouseLayout(*warehouse)
+		warehouse.Layout[positionPastObject] = object
+		newRobotPosition := warehouse.RobotPosition.GetNextMove(movement)
+		warehouse.Layout[newRobotPosition] = robot
+		warehouse.Layout[warehouse.RobotPosition] = space
+		warehouse.RobotPosition = newRobotPosition
 		// Change this to a swap
-		objectPosition.Move(movement)
-		warehouse.RobotPosition.Move(movement)
 		printWarehouseLayout(*warehouse)
 
 		// pushObject(warehouse, movement, newPosition)
 		// warehouse.RobotPosition = newPosition
 	case wall:
-		log.Println("We have an wall space")
+		log.Println("We have an wall space trying to push")
 	case object:
-		// pushObject(warehouse, movement, newPosition)
+		pushObject(warehouse, movement, positionPastObject)
 
-		log.Println("We have an object space")
+		log.Println("We have an object space trying to push")
 	}
 }
 

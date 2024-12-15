65b377acc66884a3c5381468c68a1e99528efeb8
Bob Parsons <deadstylebp@gmail.com>
2024-12-15 17:42:20 -0600

    working day15a

--- days/day15.go
+++ days/day15.go
@@ -7,32 +7,6 @@ import (
 	"os"
 )
 
-// type Movement Position
-//
-// type Movement struct{
-//  Up Position
-//
-// Down	Position
-//  Left Position
-//  Right Position
-//
-// }
-
-type Warehouse struct {
-	Layout        map[Position]rune
-	Movements     []Position
-	RowLen        int
-	ColLen        int
-	RobotStarting Position
-	RobotPosition Position
-}
-
-func Day15() {
-	warehouse := getDay15Data()
-	log.Println(warehouse)
-	moveWarehouseRobot(&warehouse)
-}
-
 var (
 	space  = rune(46)
 	wall   = rune(35)
@@ -40,17 +14,44 @@ var (
 	robot  = rune(64)
 )
 
+type Warehouse struct {
+	Layout         map[Position]rune
+	LayoutExpanded map[Position]rune
+	Movements      []Position
+	RowLen         int
+	ColLen         int
+	RobotStarting  Position
+	RobotPosition  Position
+}
+
+func Day15() {
+	warehouse := getDay15Data()
+	moveWarehouseRobot(&warehouse)
+
+	gpsCordinates := calculateGPSCoordinates(warehouse)
+	log.Printf("Laternfish GPS Coordinates: %d ", gpsCordinates)
+}
+
+func calculateGPSCoordinates(warehouse Warehouse) int {
+	results := 0
+	for key, value := range warehouse.Layout {
+		if value == object {
+			result := (key.Row * 100) + key.Col
+			results += result
+		}
+	}
+
+	return results
+}
+
 func moveWarehouseRobot(warehouse *Warehouse) {
-	for i, movement := range warehouse.Movements {
-		log.Println("Starting movements", i, movement)
+	for _, movement := range warehouse.Movements {
 		handleNextRobotMove(warehouse, movement)
-		// if i > 10 {
-		// 	break
-		// }
 	}
 }
 
 func printWarehouseLayout(warehouse Warehouse) {
+	return
 	result := ""
 	for i := 0; i < warehouse.RowLen; i++ {
 		for j := 0; j < warehouse.ColLen; j++ {
@@ -67,20 +68,13 @@ func handleNextRobotMove(warehouse *Warehouse, movement Position) {
 	newSpace := warehouse.Layout[newPosition]
 	switch newSpace {
 	case space:
-		log.Println("We have an open space", newPosition)
 		warehouse.Layout[warehouse.RobotPosition] = space
 		warehouse.Layout[newPosition] = robot
 		warehouse.RobotPosition = newPosition
 		printWarehouseLayout(*warehouse)
-		// handleNextRobotMove(warehouse, movement)
-	// case wall:
-	// 	log.Println("We have an wall space")
 	case object:
-		log.Println("We have an object space")
 		pushObject(warehouse, movement, newPosition)
-
 	}
-	// printWarehouseLayout(*warehouse)
 }
 
 func pushObject(warehouse *Warehouse, movement Position, objectPosition Position) {
@@ -88,24 +82,15 @@ func pushObject(warehouse *Warehouse, movement Position, objectPosition Position
 	spacePastObject := warehouse.Layout[positionPastObject]
 	switch spacePastObject {
 	case space:
-		log.Println("We have an open space when trying to push", objectPosition)
 		printWarehouseLayout(*warehouse)
 		warehouse.Layout[positionPastObject] = object
 		newRobotPosition := warehouse.RobotPosition.GetNextMove(movement)
 		warehouse.Layout[newRobotPosition] = robot
 		warehouse.Layout[warehouse.RobotPosition] = space
 		warehouse.RobotPosition = newRobotPosition
-		// Change this to a swap
 		printWarehouseLayout(*warehouse)
-
-		// pushObject(warehouse, movement, newPosition)
-		// warehouse.RobotPosition = newPosition
-	case wall:
-		log.Println("We have an wall space trying to push")
 	case object:
 		pushObject(warehouse, movement, positionPastObject)
-
-		log.Println("We have an object space trying to push")
 	}
 }
 
@@ -113,13 +98,8 @@ func (position Position) GetNextMove(direction Position) Position {
 	return Position{position.Row + direction.Row, position.Col + direction.Col}
 }
 
-func (position *Position) Move(direction Position) {
-	position.Row = position.Row + direction.Row
-	position.Col = position.Col + direction.Col
-}
-
 func getDay15Data() Warehouse {
-	file, err := os.Open("inputs/test.txt")
+	file, err := os.Open("inputs/input15.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -144,9 +124,9 @@ func getDay15Data() Warehouse {
 
 		switch emptySpaceFound {
 		case false:
-			for col, space := range row {
-				warehouse.Layout[Position{warehouse.RowLen, col}] = space
-				if space == 64 { // Robot is a @
+			for col, location := range row {
+				warehouse.Layout[Position{warehouse.RowLen, col}] = location
+				if location == robot {
 					warehouse.RobotStarting = Position{warehouse.RowLen, col}
 					warehouse.RobotPosition = Position{warehouse.RowLen, col}
 				}
